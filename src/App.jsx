27787abb73cffdb5034258d702a68de37e277886
aloha.quan <aloha.quando@gmail.com>
2022-08-27 20:33:55 +0700

    First commit, added mongoose, tailwindcss, vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center py-4">
+      <h1 className="text-3xl font-semibold">FWP Final Group Assessment</h1>
+    </div>
+  );
+}
+
+export default App;
