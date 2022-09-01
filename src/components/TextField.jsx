680eeb8a7b520a78c82a7188b90485d4fbe7d201
Quan Do <aloha.quando@gmail.com>
2022-09-01 17:41:48 +0700

    Added pages and installed more modules
    Added Sign In, Sign Up, Home, Page Not Found pages
    Installed axios, flowbite, hookrouter

---
+++ src/components/TextField.jsx
@@ -0,0 +1,3 @@
+export default function TextField() {
+  const [input, setInput] = useState
+}
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,22 @@
+import { A } from "hookrouter";
+import pageNotFoundIllustration from "../assets/404-bot.svg";
+
+export default function NotFoundPage() {
+  return (
+    <div className=" bg-brand-blue-gray-50 min-h-screen text-center space-y-4 flex flex-col justify-center">
+      <img
+        src={pageNotFoundIllustration}
+        className="block max-w-md mx-auto"
+      />
+
+      <h1 className="text-3xl font-semibold">Page not found</h1>
+
+      <A
+        className="font-semibold text-white text-xl bg-brand-blue rounded-xl shadow-sm py-4 px-6 block max-w-min whitespace-nowrap mx-auto"
+        href="/"
+      >
+        Go to home
+      </A>
+    </div>
+  );
+}
