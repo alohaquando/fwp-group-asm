4eddac53eb085aea7baa0cc2e580705c8bd822a5
Nhật Trần <73808962+NhaTTran-0412@users.noreply.github.com>
2022-09-14 05:49:40 +0700

    Edit files

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -1,19 +1,46 @@
-const jwt = require("jsonwebtoken");
-require('dotenv').config();
-
-module.export = function (req, res, next) {
-    const token = req.header("x-auth-token");
-
-    if(!token) {
-        return res.status(401).json({msg: "No token, authorization denied"});
-    }
+app.post("/register", async (req, res) => {
 
     try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded.user;
-        next();
-
-    } catch (error) {
-        res.status(401).json({msg: "Token is not valid"})
+      // Get user input
+      const { username,password } = req.body;
+  
+      // Validate user input
+      if (!(username && password)) {
+        res.status(400).send("All input is required");
+      }
+  
+      // check if user already exist
+      // Validate if user exist in our database
+      const oldUser = await User.findOne({ email });
+  
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+  
+      //Encrypt user password
+      encryptedPassword = await bcrypt.hash(password, 10);
+  
+      // Create user in our database
+      const user = await User.create({
+        username,
+        password: encryptedPassword,
+      });
+  
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      // save user token
+      user.token = token;
+  
+      // return new user
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
     }
-}
+  });
+  
