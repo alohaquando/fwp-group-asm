3e1e27a8fdca4afa94b2dfde0c6a87193f7722d9
Quan Do <68211893+s3800978@users.noreply.github.com>
2022-09-18 02:37:18 +0700

    Guide (#13)
    * Added pages and installed more modules
    
    Added Sign In, Sign Up, Home, Page Not Found pages
    Installed axios, flowbite, hookrouter
    
    * User guide

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import NotFoundPage from "./pages/NotFoundPage";
 import { useRoutes } from "hookrouter";
 import Test from "./pages/Test";
 
+// Run this project
+// // npm install
+// // npm start
+
+// Front-end at port 5173 (default)
+// API server at port 3000
+
 const routes = {
   "/": () => <Home />,
   "/signup": () => <SignUp />,
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import "./index.css";
 
 import { Data } from "./data/data";
 
+// Run this project
+// // npm install
+// // npm start
+
+// Front-end at port 5173 (default)
+// API server at port 3000
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Data>
     <App />
